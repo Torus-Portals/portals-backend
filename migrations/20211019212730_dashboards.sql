02711ae3b0e7c4114ff1349cf938a948b2062945
Broch Stilley <brochington@gmail.com>
2021-11-22 11:54:43 -0800

    add deleted status columns to dashboards schema

--- migrations/20211019212730_dashboards.sql
+++ migrations/20211019212730_dashboards.sql
@@ -6,7 +6,9 @@ CREATE TABLE dashboards (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   created_by UUID NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_by UUID NOT NULL
+  updated_by UUID NOT NULL,
+  deleted_at TIMESTAMPTZ DEFAULT NULL,
+  deleted_by UUID DEFAULT NULL
 );
 
 SELECT sqlx_manage_updated_at('dashboards');
